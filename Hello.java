b149c29caf18fc7cce75b305d2e1580cf99964be
jastisri2023-eng <jasti.sri2023@vitstudent.ac.in>
2026-02-18 09:09:03 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,3 @@
 public class Hello {
-public static void main(String[] args) {
-System.out.println("Hello from java via Jenkins");
-}
-  }
+    public static void main(String[] args) {
+        System.out.println("Error test")
